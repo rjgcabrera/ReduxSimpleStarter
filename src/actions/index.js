ac44f03a370ee5ec6521b8aa54b2c6315a850e82
Raymond Cabrera <rjgcengr@gmail.com>
2018-06-29 14:02:32 -0700

    Return an action object from selectBook, pass in action creator to li in book-list, and change npm start script to use nodemon

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,5 +1,10 @@
 // will contain all the different action creators that we create
 
 export function selectBook(book) {
-  console.log('A book has been selected: ', book.title);
+  // console.log('A book has been selected: ', book.title);
+  // selectBook is an action creator. It needs to return an action: an object with a type property
+  return {
+    type: 'BOOK_SELECTED',
+    payload: book
+  };
 }
